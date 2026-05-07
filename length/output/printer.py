c5216b8448ffda2e067f2680eb444e3f38eb5963
mintrecc <maxdavydenkooo@gmail.com>
2026-05-07 18:16:12 +0300

    Part of logic postponed to class TextAnalyzer

--- length/output/printer.py
+++ length/output/printer.py
@@ -8,7 +8,8 @@ from line.parser.tokenizer import tokenize
 
 
 
-def print_report_in_table(text):
+
+def print_report_in_table(report):
     w1 = 27
     w2 = 8
     w3 = 6
@@ -18,12 +19,12 @@ def print_report_in_table(text):
     right_title = " Значення ".ljust(w2)
     print(f"|{left_title}|{right_title}|")
     print(border)
-    print("| Кількість слів".ljust(w1), f"| {str(count_words(text)).rjust(w3)} |")
-    print("| Cлів довжини 3".ljust(w1), f"| {str(count_words_len(text, 3)).rjust(w3)} |")
-    print("| Унікальних слів".ljust(w1), f"| {str(count_unique_words(text)).rjust(w3)} |")
-    print("| Починаються з великої".ljust(w1), f"| {str(count_titlecase_words(text)).rjust(w3)} |")
-    print("| Чисел".ljust(w1), f"| {str(count_digits(text)).rjust(w3)} |")
-    print("| Кількість знакозмін".ljust(w1), f"| {str(alternation_count(text)).rjust(w3)} |")
+    print("| Кількість слів".ljust(w1), f"| {str(report['Кількість слів']).rjust(w3)} |")
+    print("| Cлів довжини 3".ljust(w1), f"| {str(report['Слів довжини 3']).rjust(w3)} |")
+    print("| Унікальних слів".ljust(w1), f"| {str(report['Унікальних слів']).rjust(w3)} |")
+    print("| Починаються з великої".ljust(w1), f"| {str(report['Починаються з великої']).rjust(w3)} |")
+    print("| Чисел".ljust(w1), f"| {str(report['Чисел']).rjust(w3)} |")
+    print("| Кількість знакозмін".ljust(w1), f"| {str(report['Кількість знакозмін']).rjust(w3)} |")
     print(border)
 
 
--- main.py
+++ main.py
@@ -1,13 +1,55 @@
 import sys
 
-from clining import analysis
+
 from length.output.printer import *
 from line.config import separate_by_sentence
 from line.parser.tokenizer import tokenize_by_sentence
 
+class TextAnalyzer:
+    def __init__(self, text):
+        self.text = text
+
+        self.sentences = tokenize_by_sentence(self.text)
+
+        self.tokens = tokenize(self.text)
+
+    def create_a_report(self):
+        counted_words = count_words(self.tokens)
+        counted_words_len = count_words_len(self.tokens, 3)
+        counted_unique_words = count_unique_words(self.tokens)
+        counted_titlecase_words = count_titlecase_words(self.tokens)
+        counted_digits = count_digits(self.tokens)
+        alternation_counted = alternation_count(self.tokens)
+
+        report = {"Кількість слів": counted_words,
+                    "Слів довжини 3": counted_words_len,
+                    "Унікальних слів": counted_unique_words,
+                    "Починаються з великої": counted_titlecase_words,
+                    "Чисел": counted_digits,
+                    "Кількість знакозмін": alternation_counted}
+
+        return report
+
+    def create_sentence_report(self):
+        all_reports = []
+
+        for sentence in self.sentences:
+            words = tokenize(sentence)
+
+            sentence_report = {
+            "Кількість слів": count_words(words),
+            "Слів довжини 3": count_words_len(words, 3),
+            "Унікальних слів": count_unique_words(words),
+            "Починаються з великої": count_titlecase_words(words),
+            "Чисел": count_digits(words),
+            "Кількість знакозмін": alternation_count(words)
+            }
+
+            all_reports.append(sentence_report)
+
+        return all_reports
 
 def main():
-
 
     if len(sys.argv) < 1:
         print('No arguments')
@@ -25,19 +67,17 @@ def main():
     else:
         text = " ".join(sys.argv[1:])
 
+    analyzer = TextAnalyzer(text)
+
     if separate_by_sentence:
-        sentences = tokenize_by_sentence(text)
-        print(sentences)
-        for sentence in sentences:
-            words = tokenize(sentence)
+        sentences_report = analyzer.create_sentence_report()
+        for report in sentences_report:
             print("\n")
-            print_report_in_table(words)
-
-
+            print_report_in_table(report)
 
     if not separate_by_sentence:
-        words = tokenize(text)
-        print_report_in_table(words)
+        full_report = analyzer.create_a_report()
+        print_report_in_table(full_report)
 
 if __name__ == '__main__':
     main()
